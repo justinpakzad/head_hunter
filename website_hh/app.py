98a32a8d04ec6a5aab0323a5f2139ec7558b09f7
Justin Pakzad <justinpak303@gmail.com>
2023-03-13 12:10:48 +0100

    bounding boxes fixed

--- website_hh/app.py
+++ website_hh/app.py
@@ -21,48 +21,30 @@ project = rf.workspace().project('crowd_counting')
 model = project.version(12).model
 
 
+
+
 def load_images(cv_image, confidence_threshold, overlap_threshold):
-
-    buffer = io.BytesIO()
-    Image.fromarray(cv_image).save(buffer, format='JPEG', quality=90)
-    buffer.seek(0)
-
-    # Base64 encode the image
-    image_str = base64.b64encode(buffer.getvalue()).decode('ascii')
-
     # Make prediction using Roboflow API
-    robo_prediction = model.predict(cv_image, confidence=confidence_threshold, overlap=overlap_threshold).json()
+    robo_prediction = model.predict(cv_image, confidence=confidence_threshold*100, overlap=overlap_threshold*100).json()
 
     st.write(f'Our system detects {len(robo_prediction["predictions"])} humans')
 
+    pil_img_with_boxes = Image.fromarray(cv2.cvtColor(cv_image, cv2.COLOR_BGR2RGB))
+    draw = ImageDraw.Draw(pil_img_with_boxes)
     for prediction in robo_prediction['predictions']:
         label = prediction['class']
-        x = prediction['x']
-        y = prediction['y']
-        # xmax = xmin + prediction['width']
-        # ymax = ymin + prediction['height']
-        # draw.rectangle(((xmin, ymin), (xmax, ymax)), outline='red', width=3)
-        # draw.text((xmin, ymin), label, fill='red')
-        draw.text((x,y),'red')
+        xmin = prediction['x']
+        ymin = prediction['y']
+        width = prediction['width']
+        height =  prediction['height']
+        top_lx  = xmin  + prediction['width'] /2
+        top_ly = ymin + prediction['height'] /2
+        bottom_lx = xmin - prediction['width'] /2
+        bottom_ly = ymin - prediction['height'] /2
+        draw.rectangle(((top_lx, top_ly), (bottom_lx, bottom_ly)), outline='blue', width=5)
+        draw.point((xmin, ymin),'blue')
     st.image(pil_img_with_boxes, caption='Image with bounding boxes')
 
-    ## Construct the URL to retrieve image.
-    upload_url = f'https://detect.roboflow.com/crowd_counting/12?api_key={st.secrets["api_key"]}&confidence={confidence_threshold}&{overlap_threshold}=&format=image&labels=off'
-
-    ## POST to the API.
-    r = requests.post(upload_url,
-                    data=image_str,
-                    headers={
-        'Content-Type': 'application/x-www-form-urlencoded'
-    })
-
-    return r
-
-
-    folders = [os.path.basename(path) for path in glob.glob('/Users/justinpak/Desktop/roboflow_images/*') if os.path.isdir(path)]
-    confidence_threshold = st.sidebar.slider('Confidence threshold:', 0.0, 1.0, 0.3, 0.01)
-    overlap_threshold = st.sidebar.slider('Overlap threshold:', 0.0, 1.0, 0.5, 0.01)
-    selected_folder = st.sidebar.selectbox('What size crowd do you wanna see?', folders)
 
 def main():
     st.header('Head Hunter')
